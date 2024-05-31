6d4b6417f7f94e281a78c2522f38113d7f19ab35
zdn-duc-nguyen <duc.nguyen@zodinet.tech>
2024-05-31 14:12:34 +0700

    feat: create user account table

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
+
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, DatabaseModule],
+  imports: [ConfigModule.forRoot(), UserModule, DatabaseModule, AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
